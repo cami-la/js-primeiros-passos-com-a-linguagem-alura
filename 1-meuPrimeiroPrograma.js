652af119de95109c10898331d1ffacd3ff148d62
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-01-20 05:19:14 -0300

    01. Introdução a Linguagem

--- 1-meuPrimeiroPrograma.js
+++ 1-meuPrimeiroPrograma.js
@@ -1,1 +1,12 @@
-console.log("Meu Primeiro Programa");
+console.log("Meu Primeiro Programa: Trabalhando com variáveis.");
+
+const idade = 28;
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+console.log(idade*2);
+
+const idadeSomada = idade + 2;
+console.log(idadeSomada);
