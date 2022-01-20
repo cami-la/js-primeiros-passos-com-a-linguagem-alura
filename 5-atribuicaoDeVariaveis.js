3022e2b28d99c54f78893c2c394a1e7594554a0c
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-01-20 09:58:02 -0300

    03. Trabalhando com muitos elementos

--- 5-atribuicaoDeVariaveis.js
+++ 5-atribuicaoDeVariaveis.js
@@ -1,6 +1,5 @@
 console.log("Trabalhando com atribuição de variáveis")
 
-const idade = 28;
 const primeiroNome = "Camila";
 const sobrenome = "Cavalcante";
 
@@ -13,3 +12,10 @@ contador = contador + 1;
 const nomeCompleto = primeiroNome + sobrenome;
 
 console.log(nomeCompleto);
+
+let idade; //declarando variável
+idade = 26; //atribuindo valor
+
+idade = idade + 1;
+
+console.log(idade);
